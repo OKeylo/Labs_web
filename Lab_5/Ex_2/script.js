ae0801a6d11230c0705f3caef913ec37da52b81a
OKeylo <zabaev228@gmail.com>
2022-11-11 15:59:21 +0500

    changes

--- Lab_5/Ex_2/script.js
+++ Lab_5/Ex_2/script.js
@@ -1,7 +1,7 @@
 function checkTriangle() {
-  let side_1 = parseInt(document.getElementById('side1').value);
-  let side_2 = parseInt(document.getElementById('side2').value);
-  let side_3 = parseInt(document.getElementById('side3').value);
+  let side_1 = parseFloat(document.getElementById('side1').value);
+  let side_2 = parseFloat(document.getElementById('side2').value);
+  let side_3 = parseFloat(document.getElementById('side3').value);
   if ( isNaN(side_1) || isNaN(side_2) || isNaN(side_3) ) {
     alert('Такого треугольника не существует\nХотите проверить данные?');
     return false;
@@ -9,8 +9,8 @@ function checkTriangle() {
     alert('Такого треугольника не существует\nХотите проверить данные?');
     return false;
   }
-  let p = parseInt((side_1 + side_2 + side_3) / 2);
-  let s = parseInt( Math.sqrt(p * (p - side_1) * (p - side_2) * (p - side_3)) );
+  let p = parseFloat((side_1 + side_2 + side_3) / 2);
+  let s = parseFloat( Math.sqrt(p * (p - side_1) * (p - side_2) * (p - side_3)) );
   alert('Такой треугольник существует\nЕго площадь ' + s);
   return true;
 }
